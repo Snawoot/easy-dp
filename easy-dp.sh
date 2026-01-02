f31410551f2c6af55766c71e6723c7b3addc5fd9
Vladislav Yarmak <vladislav@vm-0.com>
2026-01-03 00:16:19 +0200

    WIP

---
+++ easy-dp.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# halt on any error for safety and proper pipe handling
+set -euo pipefail ; # <- this semicolon and comment make options apply
+# even when script is corrupt by CRLF line terminators (issue #75)
+# empty line must follow this comment for immediate fail with CRLF newlines
+
+arch="$(uname -m)"
+
+ensure_deps() {
+  for dep in "$@"; do
+    if ! command -v "$dep" >/dev/null 2>&1 ; then
+      >&2 echo "Unable to locate dependency: \"$dep\". Please install it."
+      exit 1
+    fi
+  done
+}
+
+ensure_deps curl
+
+# Install dumbproxy
+#
+declare -A dp_arch_map=(
+  ["x86_64"]="amd64"
+  ["i386"]="386"
+  ["i486"]="386"
+  ["i586"]="386"
+  ["i686"]="386"
+  ["aarch64"]="arm64"
+  ["armv5l"]="arm"
+  ["armv6l"]="arm"
+  ["armv7l"]="arm"
+  ["armhf"]="arm"
+)
+dp_download_url="https://github.com/SenseUnit/dumbproxy/releases/latest/download/dumbproxy.linux-${dp_arch_map[$arch]}"
+
+curl --no-progress-meter -Lo /usr/local/bin/dumbproxy "$dp_download_url"
+chmod +x /usr/local/bin/dumbproxy
